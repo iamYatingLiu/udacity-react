82dcc9f32c6da9139bae320c200765f381aed45c
yating <i.am.yating.liu@gmail.com>
2019-12-19 20:13:42 -0500

    Exercise - Functional Components

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MovieCard extends Component {
-  render() {
-    /*
-    Destructuring via ES6. We're getting the profiles, users, and movies properties
-    off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-    out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-    */
-    const { users, usersWhoLikedMovie, movieInfo } = this.props;
+const MovieCard = props => {
+  /*
+  Destructuring via ES6. We're getting the profiles, users, and movies properties
+  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
+  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
+  */
+  const { users, usersWhoLikedMovie, movieInfo } = props;
 
-    return (
-      <li key={movieInfo.id}>
-        <h2>{movieInfo.name}</h2>
-        <h3>Liked By:</h3>
+  return (
+    <li key={movieInfo.id}>
+      <h2>{movieInfo.name}</h2>
+      <h3>Liked By:</h3>
 
-        {!usersWhoLikedMovie || usersWhoLikedMovie.length === 0 ? (
-          <p>None of the current users liked this movie.</p>
-        ) : (
+      {!usersWhoLikedMovie || usersWhoLikedMovie.length === 0 ? (
+        <p>None of the current users liked this movie.</p>
+      ) : (
           <ul>
             {usersWhoLikedMovie &&
               usersWhoLikedMovie.map(userId => {
@@ -28,9 +27,8 @@ class MovieCard extends Component {
               })}
           </ul>
         )}
-      </li>
-    );
-  }
-}
+    </li>
+  );
+};
 
 export default MovieCard;
